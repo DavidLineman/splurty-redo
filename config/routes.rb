2dc095f8380150764c94a70c01cffbf4e8a5d3bb
DavidLineman <dline215@gmail.com>
2019-04-17 15:05:55 -0400

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'quotes#index'
-  resources :quotes
+  resources :quotes do
+    get :quote_id, to: 'quotes#new'
+  end
   get 'about', to: 'quotes#about'
 end
